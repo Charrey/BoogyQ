49f0030bcbfe0994cf0db607476fef28b115729c
Charrey <p.vanleeuwen@student.utwente.nl>
2016-06-14 15:04:52 +0200

    Preprocessor

---
+++ src/exceptions/ParseException.java
@@ -0,0 +1,11 @@
+package exceptions;
+
+/**
+ * Created by poesd_000 on 14/06/2016.
+ */
+public class ParseException extends Exception{
+
+    public ParseException(String message) {
+        super(message);
+    }
+}
