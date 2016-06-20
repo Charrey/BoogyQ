819cc44706ee099e54b33881b03e188fbc795475
Hans <j.h.vanderlaan@student.utwente.nl>
2016-06-20 12:03:07 +0200

    String => Commit.

--- src/checking/Type.java
+++ src/checking/Type.java
@@ -9,118 +9,86 @@ abstract public class Type {
 	public static final Type INT = new Int();
 	/** The singleton instance of the {@link Char} type. */
 	public static final Type CHAR = new Char();
+	private static final Type BSTRING = new BString();
 	/** The singleton instance of the {@link String} type. */
 
 	private final TypeKind kind;
 
 	/** Constructor for subclasses. */
-	protected Type(TypeKind kind) {
-		this.kind = kind;
-	}
+			protected Type(TypeKind kind) {
+				this.kind = kind;
+			}
 
-	/** Returns the kind of this type. */
-	public TypeKind getKind() {
-		return this.kind;
-	}
-
-	/** returns the size (in bytes) of a value of this type. */
-	abstract public int size();
-
-	/** Representation of the BoogyQ Boolean type. */
-	static public class Bool extends Type {
-		private Bool() {
-			super(TypeKind.BOOL);
+			/** Returns the kind of this type. */
+		public TypeKind getKind() {
+			return this.kind;
 		}
 
-		@Override
-		public int size() {
-			return 4; //TODO: Store all these things somewhere where all the information of the machine is.
+		/** Representation of the BoogyQ Boolean type. */
+		static public class Bool extends Type {
+			private Bool() {
+				super(TypeKind.BOOL);
+			}
+
+			public int size() {
+				return 4; //TODO: Store all these things somewhere where all the information of the machine is.
+			}
+
+			@Override
+			public java.lang.String toString() {
+				return "Boolean";
+			}
 		}
 
-		@Override
-		public java.lang.String toString() {
-			return "Boolean";
-		}
-	}
+		/** Representation of the BoogyQ Integer type. */
+		static public class Int extends Type {
+			private Int() {
+				super(TypeKind.INT);
+			}
 
-	/** Representation of the BoogyQ Integer type. */
-	static public class Int extends Type {
-		private Int() {
-			super(TypeKind.INT);
+			public int size() {
+				return 4;
+			}
+
+			@Override
+			public java.lang.String toString() {
+				return "Integer";
+			}
 		}
 
-		@Override
-		public int size() {
-			return 4;
-		}
+		/** Representation of the BoogyQ Char type. */
+		static public class Char extends Type {
+			private Char() {
+				super(TypeKind.CHAR);
+			}
 
-		@Override
-		public java.lang.String toString() {
-			return "Integer";
+			public int size() {
+				return 4;
+			}
+
+			@Override
+			public java.lang.String toString() {
+				return "Char";
+			}
 		}
-	}
 
 	/** Representation of the BoogyQ Char type. */
-	static public class Char extends Type {
-		private Char() {
-			super(TypeKind.CHAR);
-		}
 
-		@Override
-		public int size() {
-			return 4;
-		}
-
-		@Override
-		public java.lang.String toString() {
-			return "Char";
-		}
-	}
-
-	/** Representation of the BoogyQ Char type. */
 	static public class BString extends Type {
-		private final int lower;
-		private final int upper;
-
-		private BString(int lower, int upper) {
+		private BString() {
 			super(TypeKind.STRING);
-			assert upper >= lower;
-			this.lower = lower;
-			this.upper = upper;
-		}
-
-		/** Returns the lower bound of this String type. */
-		public int getLower() {
-			return this.lower;
-		}
-
-		/** Returns the upper bound of this String type. */
-		public int getUpper() {
-			return this.upper;
-		}
-
-		/** Returns the element bound of this array type. */
-		public Type getElemType() {
-			return Type.CHAR;
-		}
-
-		@Override
-		public int size() {
-			return (getUpper() - getLower() + 1) * Type.CHAR.size();
 		}
 
 		@Override
 		public String toString() {
-			return "String [" + this.lower + ".." + this.upper + "]";
+			return "String";
 		}
 
 		@Override
-		public int hashCode() {
+			public int hashCode() {
 			final int prime = 31;
 			int result = 1;
-			result = prime * result + Type.CHAR.hashCode();
-			result = prime * result + this.lower;
-			result = prime * result + this.upper;
+			result = prime * result + Type.BSTRING.hashCode();
 			return result;
 		}
 
@@ -132,13 +100,6 @@ abstract public class Type {
 			if (!(obj instanceof BString)) {
 				return false;
 			}
-			BString other = (BString) obj;
-			if (this.lower != other.lower) {
-				return false;
-			}
-			if (this.upper != other.upper) {
-				return false;
-			}
 			return true;
 		}
 	}
@@ -146,42 +107,21 @@ abstract public class Type {
 
 	/** Representation of BoogyQ Array types. */
 	static public class Array extends Type {
-		private final int lower;
-		private final int upper;
 		private final Type elemType;
 
 		private Array(int lower, int upper, Type elemType) {
 			super(TypeKind.ARRAY);
 			assert upper >= lower;
-			this.lower = lower;
-			this.upper = upper;
 			this.elemType = elemType;
 		}
-
-		/** Returns the lower bound of this array type. */
-		public int getLower() {
-			return this.lower;
-		}
-
-		/** Returns the upper bound of this array type. */
-		public int getUpper() {
-			return this.upper;
-		}
-
 		/** Returns the element bound of this array type. */
 		public Type getElemType() {
 			return this.elemType;
 		}
 
 		@Override
-		public int size() {
-			return (getUpper() - getLower() + 1) * this.elemType.size();
-		}
-
-		@Override
 		public String toString() {
-			return "Array [" + this.lower + ".." + this.upper + "] of "
-					+ this.elemType;
+			return "Array of " + this.elemType;
 		}
 
 		@Override
@@ -189,8 +129,6 @@ abstract public class Type {
 			final int prime = 31;
 			int result = 1;
 			result = prime * result + this.elemType.hashCode();
-			result = prime * result + this.lower;
-			result = prime * result + this.upper;
 			return result;
 		}
 
@@ -204,12 +142,6 @@ abstract public class Type {
 			}
 			Array other = (Array) obj;
 			if (!this.elemType.equals(other.elemType)) {
-				return false;
-			}
-			if (this.lower != other.lower) {
-				return false;
-			}
-			if (this.upper != other.upper) {
 				return false;
 			}
 			return true;
