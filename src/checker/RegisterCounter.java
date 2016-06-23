f94b0b56f762100f73e635263b7900fd8f6e5f10
Charrey <p.vanleeuwen@student.utwente.nl>
2016-06-23 11:57:57 +0200

    CoMmIt

--- src/checker/RegisterCounter.java
+++ src/checker/RegisterCounter.java
@@ -1,12 +1,9 @@
 package checker;
 
 import org.antlr.v4.runtime.tree.ParseTree;
-import org.antlr.v4.runtime.tree.ParseTreeWalker;
+import org.antlr.v4.runtime.tree.ParseTreeProperty;
 import parser.BoogyQBaseVisitor;
 import parser.BoogyQParser;
-
-import java.util.LinkedList;
-import java.util.List;
 
 /**
  * Created by poesd_000 on 21/06/2016.
@@ -17,26 +14,212 @@ public class RegisterCounter extends BoogyQBaseVisitor<Integer>{
         return input.accept(this);
     }
 
+    public ParseTreeProperty<Integer> regcount = new ParseTreeProperty<>();
+
 
     @Override
     public Integer visitProgram(BoogyQParser.ProgramContext ctx) {
         int max = 0;
         for (BoogyQParser.StatementContext i : ctx.statement()) {
-            max = Math.max(max, visit(i));
+            int regcount = visit(i);
+                max = Math.max(max, regcount);
         }
-        return max;
+        regcount.put(ctx, max+3);
+        return max + 3;
+        //max + r_standard + r_arp + r_load
     }
 
 
     @Override
     public Integer visitBoolexpr(BoogyQParser.BoolexprContext ctx) {
-        return 1;
+        regcount.put(ctx, 0);
+        return 0;
     }
 
     @Override
     public Integer visitTimesexpr(BoogyQParser.TimesexprContext ctx) {
-        return Math.max(visit(ctx.expr(0)), visit(ctx.expr(1))) + 1;
+        regcount.put(ctx, Math.max(visit(ctx.expr(0)), visit(ctx.expr(1))));
+        return regcount.get(ctx);
     }
 
+    @Override
+    public Integer visitPowerexpr(BoogyQParser.PowerexprContext ctx) {
+        regcount.put(ctx, Math.max(visit(ctx.expr(0)), visit(ctx.expr(1))));
+        return regcount.get(ctx);
+    }
 
+    @Override
+    public Integer visitPlusexpr(BoogyQParser.PlusexprContext ctx) {
+        regcount.put(ctx, Math.max(visit(ctx.expr(0)), visit(ctx.expr(1))));
+        return regcount.get(ctx);
+    }
+
+    @Override
+    public Integer visitOpenscope(BoogyQParser.OpenscopeContext ctx) {
+        regcount.put(ctx, 0);
+        return 0;
+    }
+
+    @Override
+    public Integer visitNotexpr(BoogyQParser.NotexprContext ctx) {
+        regcount.put(ctx,visit(ctx.expr()));
+        return regcount.get(ctx);
+    }
+
+    @Override
+    public Integer visitNumberexpr(BoogyQParser.NumberexprContext ctx) {
+        regcount.put(ctx, 0);
+        return 0;
+    }
+
+    @Override
+    public Integer visitMinusexpr(BoogyQParser.MinusexprContext ctx) {
+        regcount.put(ctx, 0);
+        return 0;
+    }
+
+    @Override
+    public Integer visitLoopstat(BoogyQParser.LoopstatContext ctx) {
+        regcount.put(ctx, 0);
+        return 0;
+    }
+
+    @Override
+    public Integer visitIgnoreme(BoogyQParser.IgnoremeContext ctx) {
+        regcount.put(ctx, visit(ctx.expr()));
+        return regcount.get(ctx);
+    }
+
+    @Override
+    public Integer visitIdenexpr(BoogyQParser.IdenexprContext ctx) {
+        regcount.put(ctx, 0);
+        return 0;
+    }
+
+    @Override
+    public Integer visitFlowexpr(BoogyQParser.FlowexprContext ctx) {
+        regcount.put(ctx, visit(ctx.flow()));
+        return regcount.get(ctx);
+    }
+
+    @Override
+    public Integer visitComparatorexpr(BoogyQParser.ComparatorexprContext ctx) {
+        regcount.put(ctx, Math.max(visit(ctx.expr(0)), visit(ctx.expr(1))));
+        return regcount.get(ctx);
+    }
+
+    @Override
+    public Integer visitIfstat(BoogyQParser.IfstatContext ctx) {
+        int condition = visit(ctx.expr());
+        int max = 0;
+        for (BoogyQParser.StatementContext st : ctx.statement()) {
+            max = Math.max(max, visit(st));
+        }
+        regcount.put(ctx, Math.max(condition, max) + 1);
+        return regcount.get(ctx);
+    }
+
+    @Override
+    public Integer visitFunctionvars(BoogyQParser.FunctionvarsContext ctx) {
+        regcount.put(ctx, 1);
+        return 1;
+    }
+
+    @Override
+    public Integer visitFunctiondecl(BoogyQParser.FunctiondeclContext ctx) {
+        int functionvars  = visit(ctx.functionvars());
+        int max = 0;
+        for (BoogyQParser.StatementContext st : ctx.statement()) {
+            max = Math.max(max, visit(st));
+        }
+        regcount.put(ctx, Math.max(functionvars, max));
+        return regcount.get(ctx);
+    }
+
+    @Override
+    public Integer visitFlowstat(BoogyQParser.FlowstatContext ctx) {
+        regcount.put(ctx, visit(ctx.flow()));
+        return regcount.get(ctx);
+    }
+
+    @Override
+    public Integer visitEmptystat(BoogyQParser.EmptystatContext ctx) {
+        regcount.put(ctx, 0);
+        return 0;
+    }
+
+    @Override
+    public Integer visitDeclstandardflow(BoogyQParser.DeclstandardflowContext ctx) {
+        regcount.put(ctx, visit(ctx.flow()));
+        return regcount.get(ctx);
+    }
+
+    @Override
+    public Integer visitDeclexpr(BoogyQParser.DeclexprContext ctx) {
+        regcount.put(ctx, 0);
+        return 0;
+    }
+
+    @Override
+    public Integer visitConcurrentstat(BoogyQParser.ConcurrentstatContext ctx) {
+        int max = 0;
+        for (BoogyQParser.StatementContext st : ctx.statement()) {
+            max = Math.max(max, visit(st));
+        }
+        regcount.put(ctx, max);
+        return max;
+    }
+
+    @Override
+    public Integer visitCommentstat(BoogyQParser.CommentstatContext ctx) {
+        regcount.put(ctx, visit(ctx.statement()));
+        return regcount.get(ctx);
+    }
+
+    @Override
+    public Integer visitClosescope(BoogyQParser.ClosescopeContext ctx) {
+        regcount.put(ctx, 0);
+        return 0;
+    }
+
+    @Override
+    public Integer visitCharexpr(BoogyQParser.CharexprContext ctx) {
+        regcount.put(ctx, 0);
+        return 0;
+    }
+
+    @Override
+    public Integer visitBreakstat(BoogyQParser.BreakstatContext ctx) {
+        regcount.put(ctx, 0);
+        return 0;
+    }
+
+    @Override
+    public Integer visitAssignstandardflow(BoogyQParser.AssignstandardflowContext ctx) {
+        regcount.put(ctx, visit(ctx.flow()));
+        return regcount.get(ctx);
+    }
+
+    @Override
+    public Integer visitAssigngeneratorflow(BoogyQParser.AssigngeneratorflowContext ctx) {
+        regcount.put(ctx, 0);
+        return 0;
+    }
+
+    @Override
+    public Integer visitAssignfunctionflow(BoogyQParser.AssignfunctionflowContext ctx) {
+        int noflows = ctx.flow().size();
+        int maxflow = 0;
+        for (BoogyQParser.FlowContext flow : ctx.flow()) {
+            maxflow = Math.max(maxflow, visit(flow));
+        }
+        regcount.put(ctx, (noflows - 1) + maxflow);
+        return regcount.get(ctx);
+    }
+
+    @Override
+    public Integer visitAndorexpr(BoogyQParser.AndorexprContext ctx) {
+        regcount.put(ctx, Math.max(visit(ctx.expr(0)), visit(ctx.expr(1))));
+        return regcount.get(ctx);
+    }
 }
---
+++ src/checker/test/RegisterCountTest.java
@@ -0,0 +1,96 @@
+package checker.test;
+
+import checker.DeclChecker;
+import checker.JumpChecker;
+import checker.RegisterCounter;
+import checker.TypeChecker;
+import exceptions.ParseException;
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.junit.Before;
+import org.junit.Test;
+import parser.BoogyQLexer;
+import parser.BoogyQParser;
+import preprocessor.PreProcessor;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+/**
+ * Created by poesd_000 on 23/06/2016.
+ */
+public class RegisterCountTest {
+
+    private RegisterCounter rc;
+
+    @Before
+    public void init() {
+        rc = new RegisterCounter();
+    }
+
+
+
+    @Test
+    public void testProg() {
+        assertEquals(rc.check(parse("5 -> int a.")), 3);
+        assertEquals(rc.check(parse("5 -> int a -> int b -> int c.")), 3);
+        assertEquals(rc.check(parse("function (int a, int b -> int c) add:\n\ta + b -> c.\n\n5, 3 => add -> int c.")), 4);
+        assertEquals(rc.check(parse("5 -> int a.\n10 -> int b.\n\nif (b>a):\n\tb + 1 -> b.\n\t<loop.\n\n5->a.\n10->b.\nif (True):\n\tb-1->b.\n\tif (b<10):\n\t\tb+1->b.\n\t<-  loop.\n\na=>showV.\nb=>showV.")), 3);
+    }
+
+
+    private ParseTree parse(String text) {
+
+        try {
+            text = PreProcessor.process(text);
+        } catch (ParseException e) {
+            fail();
+        }
+        boolean haserrors = false;
+        CharStream stream = new ANTLRInputStream(text);
+        Lexer lexer = new BoogyQLexer(stream);
+        TokenStream tokens = new CommonTokenStream(lexer);
+        BoogyQParser parser = new BoogyQParser(tokens);
+        BoogyQParser.ProgramContext tree = parser.program();
+        
+        List<String> errors = null;
+        if (!haserrors) {
+            errors = new DeclChecker().check(tree);
+            if (!errors.isEmpty()) {
+                haserrors = true;
+                for (String error : errors) {
+                    System.out.println(error);
+                }
+            }
+        }
+
+        if (!haserrors) {
+            errors = new JumpChecker().check(tree);
+            if (!errors.isEmpty()) {
+                haserrors = true;
+                for (String error : errors) {
+                    System.out.println(error);
+                }
+            }
+        }
+
+        if (!haserrors) {
+            errors = new TypeChecker().check(tree);
+            if (!errors.isEmpty()) {
+                haserrors = true;
+                for (String error : errors) {
+                    System.out.println(error);
+                }
+            }
+        }
+        if (haserrors) {
+            System.out.println("Could not compile, program had errors.");
+        } else {
+            System.out.println("Program has no errors.");
+        }
+
+        return tree;
+    }
+}
