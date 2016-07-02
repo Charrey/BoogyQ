4017f9e31208d6c0987d91e347730ca1acd6dde1
Charrey <p.vanleeuwen@student.utwente.nl>
2016-07-02 22:47:16 +0200

    fwe

--- src/toplevel/CodeSnippetTester.java
+++ src/toplevel/CodeSnippetTester.java
@@ -5,6 +5,7 @@ import divider.DividerResult;
 import exceptions.CompileError;
 import exceptions.parser.ParseException;
 import org.antlr.v4.runtime.tree.ParseTree;
+import org.junit.Test;
 import preprocessor.PreProcessor;
 import sprocl.Assembler;
 import sprocl.model.Program;
@@ -18,6 +19,8 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
 
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by Administrator on 28-6-2016.
  */
@@ -28,6 +31,91 @@ public class CodeSnippetTester {
 
     public static void main(String[] args) throws IOException {
         System.out.println(getResult("src/toplevel/gcd.boog"));
+    }
+
+    private String getLocalMem(String input) {
+        return input.split("localMem = ")[1];
+    }
+
+    @Test
+    public void test1() throws IOException {
+        String filename = "";
+        List<String> result = getResult("");
+        String expectedMem = "[0,5,1,0,97,5,0,0,0,0,0,0,0,0,0,5]";
+        String actualMem = getLocalMem(result.get(1));
+        assertEquals(expectedMem, actualMem);
+    }
+
+    @Test
+    public void test2() throws IOException {
+        String filename = "";
+        List<String> result = getResult("");
+        String expectedMem = "[0,5,5,1,1,97,97,97,97,0,0,0,0,0,0,97]";
+        String actualMem = getLocalMem(result.get(1));
+        assertEquals(expectedMem, actualMem);
+    }
+
+    @Test
+    public void test3() throws IOException {
+        String filename = "";
+        List<String> result = getResult("");
+        String expectedMem = "[0,14,-11,0,0,-75,-4802,8,0,0,0,0,5,-11,3,8]";
+        String actualMem = getLocalMem(result.get(1));
+        assertEquals(expectedMem, actualMem);
+    }
+
+    @Test
+    public void test5() throws IOException {
+        String filename = "";
+        List<String> result = getResult("");
+        String expectedMem = "[0,0,1,0,1,1,1,1,1,1,1,1,1,0,0,0]";
+        String actualMem = getLocalMem(result.get(1));
+        assertEquals(expectedMem, actualMem);
+    }
+
+    @Test
+    public void test19() throws IOException {
+        String filename = "";
+        List<String> result = getResult("");
+        String expectedMem = "[0,0,1,0,1,1,1,1,1,1,1,0,1,1,1,1]";
+        String actualMem = getLocalMem(result.get(1));
+        assertEquals(expectedMem, actualMem);
+    }
+
+    @Test
+    public void test6() throws IOException {
+        String filename = "";
+        List<String> result = getResult("");
+        String expectedMem = "[0,65,98,0,98,98,1,1,1,1,98,1,1,0,65,0]";
+        String actualMem = getLocalMem(result.get(1));
+        assertEquals(expectedMem, actualMem);
+    }
+
+    @Test
+    public void test7() throws IOException {
+        String filename = "";
+        List<String> result = getResult("");
+        String expectedMem = "[0,2,5,1,1,0,0,0,0,0,0,0,0,2,1,5]";
+        String actualMem = getLocalMem(result.get(1));
+        assertEquals(expectedMem, actualMem);
+    }
+
+    @Test
+    public void test9() throws IOException {
+        String filename = "";
+        List<String> result = getResult("");
+        String expectedMem = "[0,5,5,5,0,0,0,0,0,0,0,0,0,0,0,1]";
+        String actualMem = getLocalMem(result.get(1));
+        assertEquals(expectedMem, actualMem);
+    }
+
+    @Test
+    public void test17() throws IOException {
+        String filename = "";
+        List<String> result = getResult("");
+        String expectedMem = "[0,1,1,1,1,1,1,1,0,0,0,0,0,3,9765625,1]";
+        String actualMem = getLocalMem(result.get(1));
+        assertEquals(expectedMem, actualMem);
     }
 
     public static List<String> getResult(String Filename) throws IOException {
